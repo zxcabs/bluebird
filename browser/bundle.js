307a0c3e4f4dd787356c5d9f118a59187c5b49b8
Petka Antonov <petka_antonov@hotmail.com>
2013-11-05 19:13:29 +0200

    bundle

--- browser/bundle.js
+++ browser/bundle.js
@@ -3857,24 +3857,56 @@ var util = require( "./util.js");
 var tryCatch1 = util.tryCatch1;
 var errorObj = util.errorObj;
 
-function CatchFilter( instances, callback, boundTo ) {
+function CatchFilter( instances, callback, promise ) {
     this._instances = instances;
     this._callback = callback;
-    this._boundTo = boundTo;
+    this._promise = promise;
 }
-CatchFilter.prototype.doFilter = function CatchFilter$doFilter( e ) {
-    if( e === null || typeof e !== "object" ) {
-        throw e;
+
+
+function safePredicate( predicate, e ) {
+    var safeObject = {};
+    var retfilter = tryCatch1( predicate, safeObject, e );
+
+    if( retfilter === errorObj ) return retfilter;
+
+    var safeKeys = Object.keys(safeObject);
+    if( safeKeys.length ) {
+        errorObj.e = new TypeError(
+            "Catch filter must inherit from Error "
+          + "or be a simple predicate function" );
+        return errorObj;
     }
+    return retfilter;
+}
+
+CatchFilter.prototype.doFilter = function CatchFilter$doFilter( e ) {
     var cb = this._callback;
+
     for( var i = 0, len = this._instances.length; i < len; ++i ) {
         var item = this._instances[i];
-        if( e instanceof item ) {
-            var ret = tryCatch1( cb, this._boundTo, e );
+        var itemIsErrorType = item === Error ||
+            ( item != null && item.prototype instanceof Error );
+
+        if( itemIsErrorType && e instanceof item ) {
+            var ret = tryCatch1( cb, this._promise._boundTo, e );
             if( ret === errorObj ) {
                 throw ret.e;
             }
             return ret;
+        } else if( typeof item === "function" && !itemIsErrorType ) {
+            var shouldHandle = safePredicate(item, e);
+            if( shouldHandle === errorObj ) {
+                this._promise._attachExtraTrace( errorObj.e );
+                e = errorObj.e;
+                break;
+            } else if(shouldHandle) {
+                var ret = tryCatch1( cb, this._promise._boundTo, e );
+                if( ret === errorObj ) {
+                    throw ret.e;
+                }
+                return ret;
+            }
         }
     }
     ensureNotHandled( e );
@@ -3882,6 +3914,7 @@ CatchFilter.prototype.doFilter = function CatchFilter$doFilter( e ) {
 };
 
 module.exports = CatchFilter;
+
 },{"./errors.js":26,"./util.js":52}],25:[function(require,module,exports){
 /**
  * Copyright (c) 2013 Petka Antonov
@@ -4975,15 +5008,14 @@ function Promise$catch( fn ) {
             j = 0, i;
         for( i = 0; i < len - 1; ++i ) {
             var item = arguments[i];
-            if( typeof item === "function" &&
-                ( item.prototype instanceof Error ||
-                item === Error ) ) {
+            if( typeof item === "function" ) {
                 catchInstances[j++] = item;
             }
             else {
                 var catchFilterTypeError =
                     new TypeError(
-                        "A catch filter must be an error constructor");
+                        "A catch filter must be an error constructor "
+                        + "or a filter function");
 
                 this._attachExtraTrace( catchFilterTypeError );
                 async.invoke( this._reject, this, catchFilterTypeError );
@@ -4992,7 +5024,7 @@ function Promise$catch( fn ) {
         }
         catchInstances.length = j;
         fn = arguments[i];
-        var catchFilter = new CatchFilter( catchInstances, fn, this._boundTo );
+        var catchFilter = new CatchFilter( catchInstances, fn, this );
         return this._then( void 0, catchFilter.doFilter, void 0,
             catchFilter, void 0, this.caught );
     }
@@ -8971,15 +9003,14 @@ function Promise$catch( fn ) {
             j = 0, i;
         for( i = 0; i < len - 1; ++i ) {
             var item = arguments[i];
-            if( typeof item === "function" &&
-                ( item.prototype instanceof Error ||
-                item === Error ) ) {
+            if( typeof item === "function" ) {
                 catchInstances[j++] = item;
             }
             else {
                 var catchFilterTypeError =
                     new TypeError(
-                        "A catch filter must be an error constructor");
+                        "A catch filter must be an error constructor "
+                        + "or a filter function");
 
                 this._attachExtraTrace( catchFilterTypeError );
                 async.invoke( this._reject, this, catchFilterTypeError );
@@ -8988,7 +9019,7 @@ function Promise$catch( fn ) {
         }
         catchInstances.length = j;
         fn = arguments[i];
-        var catchFilter = new CatchFilter( catchInstances, fn, this._boundTo );
+        var catchFilter = new CatchFilter( catchInstances, fn, this );
         return this._then( void 0, catchFilter.doFilter, void 0,
             catchFilter, void 0, this.caught );
     }
@@ -19682,6 +19713,23 @@ var CustomError = function(){};
 
 CustomError.prototype = new Error();
 
+var predicateFilter = function(e) {
+    return (/invalid/).test(e.message);
+}
+
+function BadError(msg) {
+    this.message = msg;
+    return this;
+}
+
+function predicatesUndefined(e) {
+    return e === void 0;
+}
+
+function predicatesPrimitiveString(e) {
+    return /^asd$/.test(e);
+}
+
 describe("A promise handler that throws a TypeError must be caught", function() {
 
     specify("in a middle.caught filter", function(done) {
@@ -19963,6 +20011,98 @@ describe("A promise handler that is caught in a filter", function() {
          }, 200 );
     });
 });
+
+describe("A promise handler with a predicate filter", function() {
+
+    specify("will catch a thrown thing matching the filter", function(done) {
+        var a = Promise.pending();
+        a.promise.then(function(){
+            throw new Error("horrible invalid error string");
+        }).caught(predicateFilter, function(e){
+            done();
+        }).caught(function(e){
+            assert.fail();
+        });
+        a.fulfill(3);
+
+    });
+    specify("will NOT catch a thrown thing not matching the filter", function(done) {
+        var a = Promise.pending();
+        a.promise.then(function(){
+            throw new Error("horrible valid error string");
+        }).caught(predicateFilter, function(e){
+            assert.fail();
+        }).caught(function(e){
+            done();
+        });
+        a.fulfill(3);
+
+    });
+
+    specify("will fail when a predicate is a bad error class", function(done) {
+        var a = Promise.pending();
+        a.promise.then(function(){
+            throw new Error("horrible custom error");
+        }).caught(BadError, function(e){
+            assert.fail();
+        }).caught(function(e){
+            // Comment-out to show the TypeError stack
+            //console.error(e.stack);
+            done();
+        });
+        a.fulfill(3);
+
+
+    });
+
+    specify("will catch a thrown undefiend", function(done){
+        var a = Promise.pending();
+        a.promise.then(function(){
+            throw void 0;
+        }).caught(function(e) { return false }, function(e){
+            assert.fail();
+        }).caught(predicatesUndefined, function(e){
+            done();
+        }).caught(function(e) {
+            assert.fail();
+        });
+        a.fulfill(3);
+    });
+
+    specify("will catch a thrown string", function(done){
+        var a = Promise.pending();
+        a.promise.then(function(){
+            throw "asd";
+        }).caught(function(e) { return false }, function(e){
+            assert.fail();
+        }).caught(predicatesPrimitiveString, function(e){
+            done();
+        }).caught(function(e) {
+            assert.fail();
+        });
+        a.fulfill(3);
+    });
+
+    specify("will fail when a predicate throws", function(done) {
+        var a = Promise.pending();
+        a.promise.then(function(){
+            throw new CustomError("error happens");
+        }).caught(function(e) { return e.f.g; }, function(e){
+            assert.fail();
+        }).caught(TypeError, function(e){
+            done();
+        }).caught(function(e) {
+            assert.fail();
+        });
+        a.fulfill(3);
+
+
+    });
+
+
+});
+
+
 },{"../../js/debug/bluebird.js":20,"assert":2}],131:[function(require,module,exports){
 "use strict";
 
